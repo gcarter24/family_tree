a0d1a05e930790a2331439d80ac672be64df8069
Chris Carter <gchriscarter@gmail.com>
2022-06-13 16:16:03 -0400

    fix first name search

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ApplicationRecord
 
 scope :ordered, -> { order(last_name: :asc) }
-scope :filter_by_first_name, -> (first_name) { where('name like ?', "%#{first_name}%")}
+scope :filter_by_first_name, -> (first_name) { where('first_name like ?', "%#{first_name}%")}
 
   def full_name
     "#{first_name} #{last_name} " 
